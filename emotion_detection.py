f7728e2296a60dccec48b3793074b616e0c2c42a
Sevak Yeressian <yeressian@example.com>
2024-08-23 15:14:31 -0400

    create emotion_detector function

---
+++ emotion_detection.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(url, json=myobj, headers=header)
+
+    formatted_response = json.loads(response.text)
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    
+    emotions_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': 0
+    }
+
+    emotions_scores['dominant_emotion'] = max(emotions_scores, key=emotions_scores.get)
+
+    return emotions_scores
+
